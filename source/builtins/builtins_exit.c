8ddd7dfd92ae5fe9da5701b96003a23f707f8c98
maperrea <maperrea@student.s19.be>
2021-10-08 00:06:22 +0200

    exit builtin

--- source/builtins/builtins_exit.c
+++ source/builtins/builtins_exit.c
@@ -6,6 +6,23 @@
 
 int	msh_builtins_exit(t_prog *prog)
 {
+	if (prog->argv[1])
+	{
+		if (!utils_is_number(prog->argv[1]))
+		{
+			fmt_fprint(2, "msh: exit: ");
+			fmt_fprint(2, prog->argv[1]);
+			fmt_fprint(2, ": numeric argument required\n");
+			exit(255);
+		}
+		else if (prog->argv[2])
+		{
+			utils_printerror("exit", "too many arguments");
+			return (1);
+		}
+		rl_clear_history();
+		exit(types_str2int(prog->argv[1]));
+	}
 	rl_clear_history();
 	exit(EXIT_SUCCESS);
 }
