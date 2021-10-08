2670d9a49c920887381f55517daaa00377be8afd
ancoulon <ancoulon@student.s19.be>
2021-10-08 03:51:29 +0200

    removed double quotes for builtin env because bash does not display them

--- source/builtins/builtins_env.c
+++ source/builtins/builtins_env.c
@@ -14,10 +14,9 @@ int	msh_builtins_env(t_prog *prog)
 		{
 			write(1, ((t_env *)l->data)->key,
 				str_len(((t_env *)l->data)->key));
-			write(1, "=\"", 2);
+			write(1, "=", 2);
 			write(1, ((t_env *)l->data)->def,
 				str_len(((t_env *)l->data)->def));
-			write(1, "\"", 1);
 			write(1, "\n", 1);
 		}
 		l = l->next;
